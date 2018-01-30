7d24c415cf39f47d38370904dc5cfe079bd3e3db
Ian Lavallee <ialavall@edu.uwaterloo.ca>
2018-01-30 13:33:07 -0500

    Second Commit - Removed comments

--- Selenium project 1.py
+++ Selenium project 1.py
@@ -9,25 +9,20 @@ from selenium import webdriver
 import pytube
 
 videos = []
-path_to_chromedriver = 'D:\ChromeDriver\chromdriver.exe' # change path as needed
 driver = webdriver.Chrome("D:\ChromeDriver\chromedriver.exe")
 url = 'https://www.youtube.com'
 driver.get("https://www.youtube.com/channel/UC-9-kyTW8ZkZNDHQJ6FgpwQ")
 driver.maximize_window()
-# driver.find_element_by_link_text("Music").click()
 videos = driver.find_elements_by_css_selector("a[id='video-title']")
 str(videos[0].get_property("href"))
 str(videos[0].get_attribute("href"))
 links = []
 for i in range (0, 5):
     links = videos[i].get_attribute("href")
-    print(links)
+    print(links) # for debugging
     yt = pytube.YouTube(links)
-    print("Reached under yt")
     yt = yt.streams.filter(only_audio=True).all()
-    print("Reached under yt strreams")
-    yt[0].download("D:\Downloads\YoutubeDownloader")
-    print("downloaded" + str(i))    
+    yt[0].download("D:\Downloads\YoutubeDownloader")   
     
     
     
